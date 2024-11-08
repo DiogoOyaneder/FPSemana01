3336cd2591133e8f1e7dbdacfe415124beb3f5b6
diogooyaneder <diogo.oyaneder@gmail.com>
2024-11-08 12:00:33 +0000

    2o comit

--- character.py
+++ character.py
@@ -1,11 +1,10 @@
 personagens = []
 defesa_big = 0
 ataque_big = 0
-nome_big_ataque = ""
-nome_big_defesa = ""
-
+nome_big_ataque = ("")
+nome_big_defesa = ("")
 for i in range(3):
-    nome = str(input(""))
+    nome = (str(input("")))
     ataque = (int(input("")))
     defesa = (int(input("")))
     personagens.append([nome,(ataque,defesa)])
@@ -15,6 +14,5 @@ for i in range(3):
     if defesa > defesa_big:
         defesa_big = defesa
         nome_big_defesa = nome
-
 print("Ataque ", nome_big_ataque, ataque_big)
 print("Defesa ", nome_big_defesa, defesa_big)
